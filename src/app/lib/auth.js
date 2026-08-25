727f39621b7d7e574d78c295375f9047541045aa
nymulislam <naymulislam241@gmail.com>
2026-08-25 07:22:21 +0600

    feat(auth): streamline database adapter configuration and ensure social providers are correctly set up

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -6,11 +6,12 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('dhia');
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    client
-  }),
-
-   emailAndPassword: { 
-    enabled: true, 
-  }, 
+    database: mongodbAdapter(db),
+    emailAndPassword: { enabled: true },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
 });
